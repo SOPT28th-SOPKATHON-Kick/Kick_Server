df9a7034f131f8f1029bca973e94ef91139d4b09
kanghanhee <kanghanhee59@gmail.com>
2021-07-14 19:35:14 +0900

    수정

--- src/api/review.ts
+++ src/api/review.ts
@@ -2,69 +2,33 @@ import express from "express";
 import config from "../config";
 import Review from "../models/Review";
 import { IReviewInputDTO } from "../interfaces/IReview";
-import { run } from "./craw";
-import { data } from "cheerio/lib/api/attributes";
-import { next } from "cheerio/lib/api/traversing";
 
 const router = express.Router();
 
-const puppeteer = require("puppeteer");
+const axios = require('axios');
 const cheerio = require('cheerio');
 
-/**
- *  @route POST api/reviews
- *  @desc Create reviews
- *  @access Private
-*/
-router.post(
-  "/",
-  //auth,
-  async (req, res) => {
-    const {
-      title,
-      endingCountry,
-      endingAirport,
-      hashtags,
-      isInstitution,
-      institutionName,
-      content,
-      user,
-    } = req.body;
+router.get('/', function(req, res, next) {
+  let url = 'https://thegift.tistory.com/232';
+  
+  axios.get(url).then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("head");
+		//each : list 마다 함수 실행, forEach와 동일
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+        //find : 해당 태그가 있으면 그 요소 반환
+          link: $(this).find('meta[property="og:url"]').attr('content'),
+          image: $(this).find('meta[property="og:image"]').attr('content'),
+          desc: $(this).find('meta[property="og:description"]').attr('content')
+      };
+    });
 
-    // Build review object
-    let reviewFields: IReviewInputDTO = {
-      //user: user.id,
-    };
-    if (title) reviewFields.title = title;
-    if (endingCountry) reviewFields.endingCountry = endingCountry;
-    if (endingAirport) reviewFields.endingAirport = endingAirport;
-    if (isInstitution) reviewFields.isInstitution = isInstitution;
-    if (institutionName) reviewFields.institutionName = institutionName;
-    if (hashtags) reviewFields.hashtags = hashtags;
-    if (content) reviewFields.content = content;
-    
-    // Build crawlingData object
-    //if (add.link) reviewFields.crawlingData.link = add.link;
-   // if (add.image) reviewFields.crawlingData.image = add.image;
-    //if (add.desc) reviewFields.crawlingData.desc = add.desc;
-
-    try {
-        run(req,res,next);
-
-      //Create
-      let review = new Review(reviewFields);
-      await review.save();
-
-      review = await Review.findOne({ _id: review.id });
-      review.crawlingData.unshift(res);
-      await review.save();
-
-      res.json(review);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server Error");
-    }
-  }
-);
+    const data = ulList.filter(n => n.title);
+    //json으로 변환하여 app으로 전송
+    return res.json(data);
+  })
+});
 
 module.exports = router;
