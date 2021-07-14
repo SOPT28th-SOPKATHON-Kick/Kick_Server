dfc8ed26778fdaa659b4d3624e8f1f70f556b13d
kanghanhee <kanghanhee59@gmail.com>
2021-07-14 22:26:15 +0900

    수정

---
+++ src/api/review.ts
@@ -0,0 +1,85 @@
+import express from "express";
+import config from "../config";
+import Review from "../models/Review";
+
+const router = express.Router();
+const axios = require("axios");
+const cheerio = require('cheerio');
+
+/**
+ *  @route POST api/reviews
+ *  @desc Create reviews
+ *  @access Private
+*/
+router.post(
+    "/",
+    auth,
+    async (req: Request, res: Response) => {
+      const {
+        title,
+        endingCountry,
+        endingAirport,
+        hashtags,
+        isInstitution,
+        institutionName,
+        content,
+        user,
+      } = req.body;
+  
+      const add = {
+        link: null,
+        desc: null,
+        image: null,
+      };
+  
+      let url = req.body.content;
+  
+      // Build review object
+      let reviewFields: IReviewInputDTO = {
+        user: user.id,
+      };
+      if (title) reviewFields.title = title;
+      if (endingCountry) reviewFields.endingCountry = endingCountry;
+      if (endingAirport) reviewFields.endingAirport = endingAirport;
+      if (isInstitution) reviewFields.isInstitution = isInstitution;
+      if (institutionName) reviewFields.institutionName = institutionName;
+      if (hashtags) reviewFields.hashtags = hashtags;
+      if (content) reviewFields.content = content;
+      
+      // Build crawlingData object
+      if (add.link) reviewFields.crawlingData.link = add.link;
+      if (add.image) reviewFields.crawlingData.image = add.image;
+      if (add.desc) reviewFields.crawlingData.desc = add.desc;
+  
+      try {
+        //Create
+        let review = new Review(reviewFields);
+        console.log("add1 = " ,add);
+        console.log(review);
+  
+        axios.get(url).then(html => {
+          const $ = cheerio.load(html.data);
+          const $bodyList = $('head');
+          $bodyList.each(function(i, elem) {
+            add.link = $(this).find('meta[property="og:url"]').attr('content'),
+            add.image = $(this).find('meta[property="og:image"]').attr('content'),
+            add.desc = $(this).find('meta[property="og:description"]').attr('content')
+  
+            console.log("addcr = " ,add);
+            review.crawlingData.unshift(add);
+            console.log("unshift= " , review);
+            console.log("unshift= " , add);
+            review.save();
+            console.log("save= " , review); 
+          });
+        }) //크롤링 끝
+       
+        res.json(review);
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+      }
+    }
+  );
+
+  module.exports = router;
